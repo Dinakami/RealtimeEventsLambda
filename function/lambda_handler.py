76817ed08cbc42584697480cb8f2f12530050e7a
Dinakami <deedeenwagu20@gmail.com>
2022-01-16 16:49:03 -0500

    event-coordinator-lambda

---
+++ function/lambda_handler.py
@@ -0,0 +1,28 @@
+import json
+# import requests
+
+
+def lambda_function(event, context):
+    """ Lambda function to coordinate where all events are received
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+        Returns a
+
+    """
+
+    return {
+        "statusCode": True
+    }
